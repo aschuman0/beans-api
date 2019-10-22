9e1191db5de88d71b1f4966a7800efb184f84761
Aaron Schuman <aaron.schuman@urbanairship.com>
2019-10-22 16:29:04 -0700

    adds hello world endpoint at GET /api/hello

---
+++ src/main/java/RestServer.java
@@ -0,0 +1,29 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class RestServer {
+    public static void main(String[] args) throws Exception {
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+
+        Server jettyServer = new Server(8080);
+        jettyServer.setHandler(context);
+
+        ServletHolder jerseyServelet = context.addServlet(
+                org.glassfish.jersey.servlet.ServletContainer.class, "/*");
+        jerseyServelet.setInitOrder(0);
+
+        jerseyServelet.setInitParameter("jersey.config.server.provider.classnames",
+                BeansApi.class.getCanonicalName());
+
+        try {
+            jettyServer.start();
+            jettyServer.join();
+        } finally {
+            jettyServer.destroy();
+        }
+    }
+}
+
