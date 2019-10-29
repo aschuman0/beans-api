bf7b4e76687dd65104d0d69ab050692a16f7de3c
Aaron Schuman <aaron.schuman@urbanairship.com>
2019-10-29 15:33:08 -0400

    move inner class down, remove setters

--- src/main/java/Bean.java
+++ src/main/java/Bean.java
@@ -9,6 +9,36 @@ public class Bean {
     private String url;
     private double rating;
 
+    private Bean() {}
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public String getSupplier() {
+        return supplier;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
     public static class Builder {
 
         private UUID id;
@@ -68,57 +98,4 @@ public class Bean {
 
     }
 
-    private Bean() {}
-
-    public UUID getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getNotes() {
-        return notes;
-    }
-
-    public void setNotes(String notes) {
-        this.notes = notes;
-    }
-
-    public String getOrigin() {
-        return origin;
-    }
-
-    public void setOrigin(String origin) {
-        this.origin = origin;
-    }
-
-    public String getSupplier() {
-        return supplier;
-    }
-
-    public void setSupplier(String supplier) {
-        this.supplier = supplier;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public double getRating() {
-        return rating;
-    }
-
-    public void setRating(double rating) {
-        this.rating = rating;
-    }
 }
